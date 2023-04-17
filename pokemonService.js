da61f721a0eedad6886efbe3edc8169a76ca12bf
AleMar <alessandromarchesin@gmail.com>
2023-04-17 19:47:43 +0200

    Thanks chat gpt!

--- pokemonService.js
+++ pokemonService.js
@@ -1,12 +1,18 @@
 async function getPokemon(pokemonId) {
-    debugger;
-    const apiUrl = `https://pokeapi.co/api/v2/pokemon/25`;
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data)
-            return data
-        })
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
+
+    try {
+        const response = await fetch(apiUrl);
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const pokemonData = await response.json();
+        return pokemonData;
+    } catch (error) {
+        console.error('There was a problem fetching the Pokemon data:', error);
+    }
 }
 
 module.exports = getPokemon;
