1e3576f0e6578eba8033a57ba021f3d34e8cba4d
AleMar <alessandromarchesin@gmail.com>
2023-04-17 19:06:57 +0200

    Commit

--- pokemonService.js
+++ pokemonService.js
@@ -1,7 +1,12 @@
 async function getPokemon(pokemonId) {
-    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonId}`;
-
-    // Add your solution here!
+    debugger;
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/25`;
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            return data
+        })
 }
 
 module.exports = getPokemon;
